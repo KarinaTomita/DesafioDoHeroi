08ec5284ec70e85dd8648da02adbf66fcbfa9ec0
Karina Tomita <167933906+KarinaTomita@users.noreply.github.com>
2024-07-05 00:37:29 -0300

    UpDesafioDoHeroi.js

--- DesafioDoHeroi.js
+++ DesafioDoHeroi.js
@@ -11,27 +11,27 @@ if (xp <= 1000){
     nivel = "Ferro"
 } 
 else {
-    if (xp >= 1001 & <= 2000){
+    if (xp >= 1001 & xp <= 2000){
     nivel = "Bronze"
     }
     else{
-        if (xp >= 2001 & <= 5000){
+        if (xp >= 2001 & xp <= 5000){
             nivel = "Prata"
         }
         else{
-            if (xp >=5001 & <= 7000){
+            if (xp >=5001 & xp <= 7000){
                 nivel = "ouro"
             }
             else{
-            if (xp >=7001 & <= 8000){
+            if (xp >=7001 & xp <= 8000){
                 nivel = "Platina"
             }
             else{
-                if (xp >=8001 & <=9000){
+                if (xp >=8001 & xp <=9000){
                     nivel = "Ascendente" 
                     }
                     else {
-                        if (xp >= 9001 & <= 10000 ){
+                        if (xp >= 9001 & xp <= 10000 ){
                             nivel = "Imortal"
                         }
                         else {
