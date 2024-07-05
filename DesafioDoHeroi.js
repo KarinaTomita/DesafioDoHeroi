cf2fc3bbb665aa1807cdbb72b22477605b8f55d8
karinatomita <karinavieiratomita@gmail.com>
2024-07-05 00:28:34 -0300

    primeira fase de teste

---
+++ DesafioDoHeroi.js
@@ -0,0 +1,51 @@
+//variaveis
+// const
+const nome = ("heroina Chocobita")
+//var
+var xp  = (7777)
+var nivel 
+
+// pros
+
+if (xp <= 1000){
+    nivel = "Ferro"
+} 
+else {
+    if (xp >= 1001 & <= 2000){
+    nivel = "Bronze"
+    }
+    else{
+        if (xp >= 2001 & <= 5000){
+            nivel = "Prata"
+        }
+        else{
+            if (xp >=5001 & <= 7000){
+                nivel = "ouro"
+            }
+            else{
+            if (xp >=7001 & <= 8000){
+                nivel = "Platina"
+            }
+            else{
+                if (xp >=8001 & <=9000){
+                    nivel = "Ascendente" 
+                    }
+                    else {
+                        if (xp >= 9001 & <= 10000 ){
+                            nivel = "Imortal"
+                        }
+                        else {
+                                nivel = "Radiante"
+                        }
+                    }
+                }
+            }
+            }
+        }
+
+    }
+
+
+//output
+
+console.log ( ("O Herói de nome") + nome + ("está no nível de") + nivel)
